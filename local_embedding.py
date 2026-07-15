89888272638110a740cc98da6847bfa6408ad8d0
MTAKATI <sandisomtakati7@gmail.com>
2026-07-15 23:52:09 +0200

    Working on LocalEmbedding class

---
+++ local_embedding.py
@@ -0,0 +1,7 @@
+import torch
+from Vector_index import VectorIndex
+
+class LocalEmbedding:
+
+    def __init__(self, model_name="sentence-transformer/all-MiniLM-L6-v2", distance_metric="cosine"):
+        pass
